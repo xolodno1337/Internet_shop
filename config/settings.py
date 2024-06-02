0cb31926468ebf56aa17f093a3249e0dcb7a5b6b
Сергей Бутаков <xolodnotchk@mail.ru>
2024-06-02 10:50:22 +0300

    Подключение к базе данных

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Internet_shop',
+        'USER': 'postgres',
+        'PASSWORD': '7856',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
