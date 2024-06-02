3da947132f4f268e584b1d14257a1aef8706e01e
Сергей Бутаков <xolodnotchk@mail.ru>
2024-06-02 14:23:53 +0300

    Настройка административной панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_name", "price", "product_category")
+    list_filter = ("product_category",)
+    search_fields = ("product_name", "product_description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "category_name")
