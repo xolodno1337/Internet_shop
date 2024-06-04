1a4f21469e84413eca019182e98a12c341bccbae
Сергей Бутаков <xolodnotchk@mail.ru>
2024-06-04 16:40:57 +0300

    Редактирование кода

--- catalog/management/commands/cat_run.py
+++ catalog/management/commands/cat_run.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read_categories():
-        with open("category.json", encoding="utf-8", errors='ignore') as file:
+        with open("category.json", encoding="utf-8", errors="ignore") as file:
             return json.load(file)
 
     @staticmethod
     def json_read_products():
-        with open("product.json", encoding="utf-8", errors='ignore') as file:
+        with open("product.json", encoding="utf-8", errors="ignore") as file:
             return json.load(file)
 
     def handle(self, *args, **options):
@@ -24,34 +24,28 @@ class Command(BaseCommand):
         category_for_create = []
 
         for category in Command.json_read_categories():
-            category_for_create.append(
-                Category(
-                    {
-                        "id": category["pk"],
-                        "category_name": category["fields"]["category_name"],
-                        "category_description": category["fields"]["category_description"],
-                    }
-                )
+            category = Category(
+                pk=category["pk"],
+                category_name=category["fields"]["category_name"],
+                category_description=category["fields"]["category_description"],
             )
-        print(category_for_create)
+            category_for_create.append(category)
+
         Category.objects.bulk_create(category_for_create)
 
         for product in Command.json_read_products():
-            product_for_create.append(
-                Product(
-                    {
-                        "id": product["pk"],
-                        "product_name": product["fields"]["product_name"],
-                        "product_description": product["fields"]["product_description"],
-                        "product_picture": product["fields"]["product_picture"],
-                        "product_category": Category.objects.get(
-                            pk=product["fields"]["product_category"]
-                        ),
-                        "price": product["fields"]["price"],
-                        "created_at": product["fields"]["created_at"],
-                        "updated_at": product["fields"]["updated_at"],
-                    }
-                )
+            product = Product(
+                pk=product["pk"],
+                product_name=product["fields"]["product_name"],
+                product_description=product["fields"]["product_description"],
+                product_picture=product["fields"]["product_picture"],
+                product_category=Category.objects.get(
+                    pk=product["fields"]["product_category"]
+                ),
+                price=product["fields"]["price"],
+                created_at=product["fields"]["created_at"],
+                updated_at=product["fields"]["updated_at"],
             )
+            product_for_create.append(product)
 
         Product.objects.bulk_create(product_for_create)
