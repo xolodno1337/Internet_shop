1ee8aac1de4c59988319078dd0ae3e057c27e4ba
Сергей Бутаков <xolodnotchk@mail.ru>
2024-06-19 13:20:08 +0300

    добавление контролера

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -12,3 +13,9 @@ def contacts(request):
         message = request.POST.get("message")
         print(f"{name} - {phone} ({message})")
     return render(request, "contacts.html")
+
+
+def product_list(request):
+    product = Product.objects.all()
+    context = {'object_list': product}
+    return render(request, "home.html", context)
