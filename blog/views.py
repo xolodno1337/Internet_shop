d532e39f936ff1ff0a88f7de85cbcd8f3de0bf8b
Сергей Бутаков <xolodnotchk@mail.ru>
2024-07-06 10:29:27 +0300

    добавление в form_valid commit=False

--- blog/views.py
+++ blog/views.py
@@ -20,10 +20,11 @@ class BlogCreateView(CreateView):
 
     def form_valid(self, form):
         if form.is_valid():
-            new_blog = form.save()
+            new_blog = form.save(commit=False)
             new_blog.slug = slugify(new_blog.title)
             new_blog.save()
         return super().form_valid(form)
+
 
 
 class BlogListView(ListView):
@@ -50,7 +51,7 @@ class BlogUpdateView(UpdateView):
 
     def form_valid(self, form):
         if form.is_valid():
-            new_blog = form.save()
+            new_blog = form.save(commit=False)
             new_blog.slug = slugify(new_blog.title)
             new_blog.save()
         return super().form_valid(form)
