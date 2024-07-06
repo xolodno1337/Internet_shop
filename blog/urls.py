2d2aff66eef4e567f9644fea4259af9d8c3420b6
Сергей Бутаков <xolodnotchk@mail.ru>
2024-07-06 10:28:52 +0300

    добавление параметров

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.urls import path
 from blog.apps import BlogConfig
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, toggle_active
+from django.conf import settings
 
 app_name = BlogConfig.name
 
@@ -11,4 +13,4 @@ urlpatterns = [
     path("edit/<int:pk>/", BlogUpdateView.as_view(), name="edit"),
     path("delete/<int:pk>/", BlogDeleteView.as_view(), name="delete"),
     path('toggle/<int:pk>/', toggle_active, name='toggle_active'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
