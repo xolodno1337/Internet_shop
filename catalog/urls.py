f15a60146e717e846259d669fc411f102567ff66
Сергей Бутаков <xolodnotchk@mail.ru>
2024-07-06 14:45:09 +0300

    добавление CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ContactTemplateView, ProductListView, ProductDetailView
+from catalog.views import ContactTemplateView, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("contacts/", ContactTemplateView.as_view(), name="contacts"),
     path("", ProductListView.as_view(), name="product_list"),
+    path("create", ProductCreateView.as_view(), name="product_create"),
+    path("update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
