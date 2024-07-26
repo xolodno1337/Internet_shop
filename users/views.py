f51c5135343fc59d3635cf32eb6621d9c4a8c647
Сергей Бутаков <xolodnotchk@mail.ru>
2024-07-26 15:50:38 +0300

    добавление UserCreateView, email_verification, reset_password

--- users/views.py
+++ users/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+import random
+import secrets
+import string
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+from users.forms import UserRegisterForm
+from users.models import User
+from config.settings import EMAIL_HOST_USER
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Перейти по ссылку для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.token = None
+    user.save()
+    return redirect(reverse('users:login'))
+
+
+def reset_password(request):
+    context = {
+            'success_message': 'Пароль успешно сброшен. Новый пароль был отправлен на адрес Вашей электронной почты.',
+        }
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        user = get_object_or_404(User, email=email)
+        characters = string.ascii_letters + string.digits
+        characters_list = list(characters)
+        random.shuffle(characters_list)
+        password = ''.join(characters_list[:10])
+        user.set_password(password)
+        user.save()
+
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Ваш пароль для доступа на наш сайт изменен:\n {password}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            )
+
+        return render(request, 'users/reset_password.html', context)
+
+    return render(request, 'users/reset_password.html')
